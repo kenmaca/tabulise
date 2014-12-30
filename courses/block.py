157cf1e9b76633a039bc01a34658087184dcf00f
ben-cooper <ben0cooper@gmail.com>
2014-12-30 18:02:51 -0500

    Ben: fixed a typo

--- courses/block.py
+++ courses/block.py
@@ -1,51 +1,53 @@
 class Block():
-    ''' This is an abstract class.  Do not instantiate! '''
-    
+    """
+    This is an abstract class.  Do not instantiate!
+    """
     def __init__(self, id, course, timeslot):
-        ''' (Class, str, str, TimeSlot) -> Class '''
-        
+        """
+        (Block, str, str, TimeSlot) -> Class
+        """
         self._id = id
         self._course = course
         self._time = timeslot
         
     def getId(self):
         """
-        (Class) -> str
+        (Block) -> str
         Return the id of this Class.
         """
         return self._id
     
     def getCourse(self):
         """
-        (Class) -> Course
+        (Block) -> Course
         Return the Course to which this class belongs.
         """
         return self._course
     
     def getTime(self):
         """
-        (Class) -> TimeSlot
+        (Block) -> TimeSlot
         Return the TimeSlot that this Class occupies.
         """
         return self._time
     
     def setId(self, id):
         """
-        (Class, str) -> NoneType
+        (Block, str) -> NoneType
         Set the id of this Class.
         """
         self._id = id
         
     def setCourse(self, course):
         """
-        (Class, Course) -> NoneType
+        (Block, Course) -> NoneType
         Set the Course to which this Class belongs.
         """
         self._course = course
         
     def setTime(self, time):
         """
-        (Class, str) -> NoneType
+        (Block, str) -> NoneType
         Set the time of this Class.
         """
         self._time = time
