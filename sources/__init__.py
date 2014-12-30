9559ce059cb74497eed6a5ec31ef27774784604f
Kenneth Ma <k.ma@utoronto.ca>
2014-12-30 16:44:21 -0500

    Template __init__.py with packages added to default path.

---
+++ sources/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# add local packages to Python path
+path = os.path.abspath(os.path.dirname(__file__))
+for subdir in ["/.."]:
+    if not (path + subdir) in sys.path:
+        sys.path.insert(0, os.path.abspath(path + subdir))
+del subdir
