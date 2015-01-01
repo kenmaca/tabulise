b67dcf413d097f0a46dc85bbc1f666ed634061ad
Kenneth Ma <k.ma@utoronto.ca>
2015-01-01 01:12:25 -0500

    Added a general utilities module - containing only blocksToCourses right now.

--- optipeach/__init__.py
+++ optipeach/__init__.py
@@ -1,6 +1,7 @@
 from . sources import *
 from . courses import *
 from . rankers import *
+from . utils import *
 
 # active scraper
 from . sources.utscscraper import *
---
+++ optipeach/utils.py
@@ -0,0 +1,25 @@
+from . courses.course import Course
+from . courses.lecture import Lecture
+from . courses.tutorial import Tutorial
+from . courses.practical import Practical
+
+def blocksToCourses(blocks):
+	''' (list of Block) -> list of Course
+	Organizes Blocks into newly created Course objects associated by 
+	Block.getCourse() as unique ID's.
+	'''
+
+	courses = {}
+
+	for block in blocks:
+		c = courses[block.getCourse()] if block.getCourse() in \
+		        courses else Course([], [], [], block.getCourse())
+
+		{Lecture: c.addLecture, 
+		 Tutorial: c.addTutorial, 
+		 Practical: c.addPractical}[type(block)](block)
+
+		# add the modified Course back into the dict
+		courses[block.getCourse()] = c
+
+	return courses
